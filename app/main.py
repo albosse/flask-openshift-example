13fe23e830f6f19a4397467eef8f2fe88d07a043
Alexander Bosse <alexander.bosse@lhind.dlh.de>
2019-03-31 20:17:56 +0200

    j

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify
 HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS', 'localhost')
 APP_NAME = os.environ.get('OPENSHIFT_APP_NAME', 'flask')
 IP = os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1')
-PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 5000))
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8000))
 HOME_DIR = os.environ.get('OPENSHIFT_HOMEDIR', os.getcwd())
 
 log = logging.getLogger(__name__)
